098d7add1d3ddebd216c11cfc9a695fe212b907e
Nikolay Petrov <78559158+niki03petrov@users.noreply.github.com>
2026-01-04 01:49:31 +0200

    changes in gameplay

--- src/sofia/sap/interview/project/game/map/Playground.java
+++ src/sofia/sap/interview/project/game/map/Playground.java
@@ -4,13 +4,16 @@ import sofia.sap.interview.project.game.map.cooridnates.Coordinates;
 import sofia.sap.interview.project.game.map.directions.Direction;
 import sofia.sap.interview.project.game.map.room.Room;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Playground {
     private final Room[][] map;
 
-    public Playground(Room[][] map) {
+    public Playground(Reader reader) {
         this.map = new Room[map.length][];
 
         for (int i = 0; i < map.length; i++) {
@@ -18,6 +21,18 @@ public class Playground {
             System.arraycopy(map[i], 0, this.map[i], 0, map[i].length);
         }
     }
+
+    private void loadMapFromFile(Reader reader) {
+        try (BufferedReader mapReader = new BufferedReader(reader)) {
+            String line;
+            while ((line = mapReader.readLine()) != null) {
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private
 
     public Set<Direction> getPossibleDirections(Coordinates coordinates) {
         Set<Direction> directions = new HashSet<>();
